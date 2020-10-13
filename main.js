dcd34ea06d42f8d86d679ed502b497a150eb9357
Stanislao <stanislaoweb@libero.it>
2020-10-13 16:23:02 +0200

    Forse ci sto capendo qualcosa

---
+++ main.js
@@ -0,0 +1,17 @@
+var kmPercorsi = prompt('Quanti km devi fare?');
+
+var anniUtente = prompt('Quanti anni hai?');
+
+var minorenne = 18;
+
+var anziano = 65;
+
+if (!isNaN(anniUtente)) {
+  console.log('è un numero');
+} else {
+  console.log('Non è un numero');
+}
+
+var prezzoViaggio = (kmPercorsi * 0,21);
+
+console.log(prezzoViaggio);
